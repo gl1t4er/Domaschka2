4e90c136aa88b52ba1d31b4e15f5f59325bc6720
gl1t4er <32144452+gl1t4er@users.noreply.github.com>
2017-10-17 22:27:29 +0300

    inheritance classes

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Black Pearl";
+            string name2 = "Piligrimm";
+            string name3 = "Eclipse";
+            int w2 = 22;
+            int w = 44;
+            int l = 162;
+            int l1 = 56;
+            int w1 = 25;
+            BaseShip z = new BaseShip(name1, l, w);
+            Ship1 p = new Ship1(name2, l1, w1);
+            Ship2 x = new Ship2(name3, l, w2);
+            string toPrint = z.PrintInformation();
+            Console.WriteLine(toPrint);
+            string toPrint1 = p.PrintInformation();
+            Console.WriteLine(toPrint1);
+            string toPrint2 = x.PrintInformation();
+            Console.WriteLine(toPrint2);
+
+        }
+    }
+}
---
+++ Ship1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classsi
+{
+    public class Ship1 : BaseShip
+    {
+        public Ship1 (string name2, int l1, int w1) : base(name2, l1, w1)
+        {
+            Name2 = name2;
+            W1 = w1;
+            L1 = l1;
+            string j = "Ширина судна " + name2 + " - " + w1 + ". Длина - " + l1;
+            Console.WriteLine(j);
+        }
+        public virtual string PrintInforamtion()
+        {
+            int s1 = L1 * W1;
+            string res = "Название корабля - " + Name2 + " Ширина - " + W1 + " Длина - " + L1;
+            return res;
+        }
+        public string Name2 { get; private set;}
+        public int W1 { get; private set;}
+        public int L1 { get; private set;}
+    }
+}
