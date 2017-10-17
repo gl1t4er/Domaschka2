5cbf28d686f5fff72e5ebc5f1de6de1c25350f85
gl1t4er <32144452+gl1t4er@users.noreply.github.com>
2017-10-17 22:15:20 +0300

    Add files via upload

---
+++ BaseShip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classsi
+{
+    public class BaseShip
+    {
+        public BaseShip(string name1, int l, int w)
+        {
+            Name1 = name1;
+            L = l;
+            W = w;
+        }
+        public string PrintInformation()
+        {
+            int s = W * L;
+            string res = "Имя = " + Name1 + " Длина = " + L + " Ширина = " + W + " Площадь = " + s;
+            return res;
+        }
+        public string Name1 { get; private set; }
+        public int L { get; private set; }
+        public int W { get; private set; }
+    }
+}
